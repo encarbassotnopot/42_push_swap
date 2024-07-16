741c9149695a1f2f93fd2f162e9a7400337960e7
Eina Safor Coma Bages <ecoma-ba@student.42barcelona.com>
2024-07-16 12:09:34 +0200

    mataré el desgraciat que va parir python

--- test.py
+++ test.py
@@ -1,84 +1,79 @@
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    test.py                                            :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: ecoma-ba <ecoma-ba@student.42barcel>       +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2024/07/11 11:51:16 by ecoma-ba          #+#    #+#              #
-#    Updated: 2024/07/15 13:30:36 by ecoma-ba         ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
-
 import sys
 
 if len(sys.argv) < 2:
-    print("Error, not enough arguments")
+	print("Error, not enough arguments")
 
 a = {'content': [int(sys.argv[i])
-                 for i in range(1, len(sys.argv))],
-     'name': 'a'}
+				 for i in range(1, len(sys.argv))],
+	 'name': 'a'}
 b = {'content': [], 'name': 'b'}
 
 def swap(stack):
-    print (f's{stack["name"]}')
-    stack['content'][0], stack['content'][1] = stack['content'][1], stack['content'][0]
-    #print(stack['content'])
+	print (f's{stack["name"]}')
+	stack['content'][0], stack['content'][1] = stack['content'][1], stack['content'][0]
+	#print(stack['content'])
 
 def rotate(stack):
-    print (f'r{stack["name"]}')
-    stack['content'].append(stack['content'].pop(0))
-    #print(stack['content'])
+	if len(stack['content']) < 2:
+		return
+	print (f'r{stack["name"]}')
+	stack['content'].append(stack['content'].pop(0))
+	#print(stack['content'])
 
 def rev_rotate(stack):
-    print (f'rr{stack["name"]}')
-    stack['content'].insert(0, stack['content'].pop())
-    #print(stack['content'])
+	if len(stack['content']) < 2:
+		return
+	print (f'rr{stack["name"]}')
+	stack['content'].insert(0, stack['content'].pop())
+	#print(stack['content'])
 
 def push(orig, dest):
-    print (f'p{dest["name"]}')
-    dest['content'].insert(0, orig['content'].pop(0))
-    #print(dest['content'])
+	print (f'p{dest["name"]}')
+	dest['content'].insert(0, orig['content'].pop(0))
+	#print(dest['content'])
 
-def sort_top(stack):
-    if stack[0] > stack[1]:
-        swap(stack)
+sorted = False
+first_el = None
+	
 
-def push_rot_smallest(orig, dest):
-    if (dest[0] < orig[0]):
-        push(orig, dest)
-    rotate(orig)
+while (a['content'][-1] < a['content'][0]):
+	rev_rotate(a)
 
-push(a, b)
-push(a, b)
-swap(b)
-swap(a)
-rotate(a)
-rotate(a)
-push(a, b)
-push(a, b)
-swap(a)
-rotate(a)
-push(b, a)
-rotate(a)
-rotate(a)
-push(b, a)
-rotate(a)
-push(a, b)
-rotate(b)
-rotate(b)
-push(a, b)
-rotate(b)
-rotate(b)
-rotate(a)
-push(b, a)
-rotate(a)
-rotate(a)
-push(b, a)
-rotate(a)
-push(b, a)
-rotate(a)
-rotate(a)
-push(b, a)
-rotate(a)
-rotate(a)
+print(a['content'])
+
+def split_a():
+	i = 0
+	while (i < len(a['content'])):
+		rotate(a)
+		i+=1
+	
+		if a['content'][i] > a['content'][i+1]:
+			rotate(a)
+			break
+
+def split_b():
+	i = 0
+	while (i < len(a['content'])):
+		push(a, b)
+		rotate(b)
+		i+=1
+		if a['content'][i] > a['content'][i+1]:
+			push(a, b)
+			rotate(b)
+			break
+
+first_el = a['content'][0]
+while not sorted:
+	split_a()
+	if first_el == a['content'][0]:
+		break
+	print(a['content'])
+	print(b['content'])
+	split_b()
+	if first_el == a['content'][0]:
+		break
+	print(a['content'])
+	print(b['content'])
+
+print(a['content'])
+print(b['content'])
