f0f1d28d43796931ed71b275f9fdf1d8334248bc
Eina <109956262+encarbassotnopot@users.noreply.github.com>
2024-08-18 19:08:22 +0200

    totes les operacions

--- operations.c
+++ operations.c
@@ -6,7 +6,7 @@
 /*   By: ecoma-ba <ecoma-ba@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/10 12:29:30 by ecoma-ba          #+#    #+#             */
-/*   Updated: 2024/08/18 18:12:05 by ecoma-ba         ###   ########.fr       */
+/*   Updated: 2024/08/18 19:07:59 by ecoma-ba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,4 +37,27 @@ void	do_rrot(t_stack *stack)
 		return ;
 	printf("rr%c\n", stack->name);
 	stack->contents = stack->contents->prev;
+}
+
+void	do_swap(t_stack *stack)
+{
+	t_node	*last;
+	t_node	*first;
+	t_node	*second;
+	t_node	*third;
+
+	if (stack->len < 2)
+		return ;
+	printf("s%c\n", stack->name);
+	last = stack->contents->prev;
+	first = stack->contents;
+	second = stack->contents->next;
+	third = stack->contents->next->next;
+	last->next = second;
+	second->prev = last;
+	second->next = first;
+	first->prev = second;
+	first->next = third;
+	third->prev = first;
+	stack->contents = second;
 }
