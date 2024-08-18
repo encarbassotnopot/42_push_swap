08e563eb52fddd239a86d583e086c4e2230cf889
Eina <109956262+encarbassotnopot@users.noreply.github.com>
2024-08-18 19:08:22 +0200

    totes les operacions

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: ecoma-ba <ecoma-ba@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/10 10:35:26 by ecoma-ba          #+#    #+#             */
-/*   Updated: 2024/08/18 18:54:26 by ecoma-ba         ###   ########.fr       */
+/*   Updated: 2024/08/18 19:07:24 by ecoma-ba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,6 +52,10 @@ int	main(int argc, char **argv)
 	do_rrot(a);
 	do_rrot(a);
 	do_rrot(a);
+	print_stack(a);
+	print_stack(b);
+	do_swap(a);
+	do_swap(b);
 	print_stack(a);
 	print_stack(b);
 	free_stack(&a);
