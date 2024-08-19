ec871d0337c0b4749102e7c3fbd828db9da77a9b
Eina Safor Coma Bages <ecoma-ba@student.42barcelona.com>
2024-08-19 17:34:07 +0200

    comencem a processar input

--- build/operations.d
+++
@@ -1,3 +0,0 @@
-build/operations.o: operations.c push_swap.h \
-  /home/eina/Desktop/push_swap/ft_printf/ft_printf.h \
-  /home/eina/Desktop/push_swap/libft/libft.h
--- build/stack_utils.d
+++
@@ -1,3 +0,0 @@
-build/stack_utils.o: stack_utils.c push_swap.h \
-  /home/eina/Desktop/push_swap/ft_printf/ft_printf.h \
-  /home/eina/Desktop/push_swap/libft/libft.h
