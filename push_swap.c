9909f80c5156711f88096aeb13c489a8d4b48bdc
Eina Safor Coma Bages <ecoma-ba@student.42barcelona.com>
2024-08-21 10:50:22 +0200

    seguim amb quicksort a 3. bug arreglat (doble free amb input invàlid

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: ecoma-ba <ecoma-ba@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/10 10:35:26 by ecoma-ba          #+#    #+#             */
-/*   Updated: 2024/08/20 15:56:25 by ecoma-ba         ###   ########.fr       */
+/*   Updated: 2024/08/21 10:47:27 by ecoma-ba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,17 +30,38 @@ void	gen_order(int **nums, unsigned int *order, unsigned int len)
 	}
 }
 
-void quick_sort(t_stack orig, t_stack other, int pos, unsigned int start, unsigned int len)
+void	quick_sort(t_stack *stacks[], int pos, unsigned int start,
+		unsigned int len)
 {
+	int		stop;
+	int		i;
+	t_node	iter;
 
+	i = -1;
+	unsigned int limits = {start + len * 1 / 3, start + len * 2 / 3, start + len}
+	iter = stacks[STACK(pos)]->contents;
+	stop = iter->prev->final_pos;
+	while (++i < len)
+	{
+		if (iter->final_pos < start + len * 1 / 3)
+		{
+			do_push(stacks[STACK(pos)], stacks[OTHER(pos)]);
+			do_rot(stacks[OTHER(pos)]);
+		}
+		else if (iter->final_pos < start + len * 2 / 3)
+			do_push(stacks[STACK(pos)], stacks[OTHER(pos)]);
+		else
+			do_rot(stacks[STACK(pos)]);
+	}
+	quick_sort(stacks, S_MAX(i), start + len * 2 / 3, len - len * 2 / 3);
+	quick_sort(stacks, S_MID(i), start + len * 1 / 3, len * 2 / 3 - len * 1 / 3);
+	quick_sort(stacks, S_MIN(i), start, len * 1 / 3);
 }
-
 
 int	main(int argc, char **argv)
 {
 	t_node			*node;
-	t_stack			*a;
-	t_stack			*b;
+	t_stack			*stacks[2];
 	int				**nums;
 	unsigned int	*order;
 	int				i;
@@ -58,37 +79,19 @@ int	main(int argc, char **argv)
 		exit(1);
 	}
 	gen_order(nums, order, len);
-	a = create_stack('a');
-	b = create_stack('b');
+	stacks[0] = create_stack('a');
+	stacks[1] = create_stack('b');
 	i = -1;
 	while (nums[++i])
 	{
 		node = create_node(*nums[i], order[i]);
 		if (node)
-			append_node(node, a);
+			append_node(node, stacks[0]);
 	}
 	free(order);
 	ft_free_arr((void **)nums);
-	print_stack(a);
-	print_stack(b);
-	/*do_push(a, b);*/
-	/*do_push(a, b);*/
-	/*do_push(a, b);*/
-	/*print_stack(a);*/
-	/*print_stack(b);*/
-	/*do_rot(a);*/
-	/*do_rot(a);*/
-	/*print_stack(a);*/
-	/*print_stack(b);*/
-	/*do_rrot(a);*/
-	/*do_rrot(a);*/
-	/*do_rrot(a);*/
-	/*print_stack(a);*/
-	/*print_stack(b);*/
-	/*do_swap(a);*/
-	/*do_swap(b);*/
-	/*print_stack(a);*/
-	/*print_stack(b);*/
-	free_stack(&a);
-	free_stack(&b);
+	print_stack(stacks[0]);
+	print_stack(stacks[1]);
+	free_stack(&stacks[0]);
+	free_stack(&stacks[1]);
 }
